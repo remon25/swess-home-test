9a85f4508f0295396d63c6d87be3e2233e9d5c76
remon25 <70450564+remon25@users.noreply.github.com>
2023-12-11 00:05:10 +0200

    ds

--- src/views/HomeMobile.vue
+++ src/views/HomeMobile.vue
@@ -87,8 +87,8 @@ export default {
   z-index: 9999;
 }
 .mobile-container {
-  height: calc(100vh - 215px);
-  height: calc(100svh - 215px);
+  height: calc(100vh - 220px);
+  height: calc(100svh - 220px);
   padding: 0px;
 }
 .button_container {
