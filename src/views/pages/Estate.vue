3955dbdaee1c41e1a15940673a8c2108647a465c
remon25 <70450564+remon25@users.noreply.github.com>
2023-12-24 21:11:13 +0200

    gffg

--- src/views/pages/Estate.vue
+++ src/views/pages/Estate.vue
@@ -745,6 +745,7 @@
               <div class="estate-map-nearpy d-text-light pa-3">
                 {{ place }}
               </div>
+              <div>{{item.nearby_places}}</div>
             </v-col>
           </v-row>
           <iframe
